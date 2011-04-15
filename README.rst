56621196cd1449cd0082997feac208a96f0685c3
Tomaz Kovacic <tomaz.kovacic@gmail.com>
2011-04-15 15:46:54 +0200

    DRY cd-ing decorator

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-Fabric fabfile for setting up the dataset hosting infrastructure 
+Fabric fabfile script for setting up the dataset hosting infrastructure 
 on a linux server.
 
 See: https://github.com/tomazk/Text-Extraction-Evaluation
--- src/fabfile.py
+++ src/fabfile.py
@@ -5,18 +5,25 @@ import settings
 env.host_string = settings.HOST_STRING
 env.password = settings.PASS
 
+def gitdir(fun):
+    # decorator for cd-ing into git repository directory
+    def wrapper(*args, **kwargs):
+        with cd(settings.GIT_DIR):
+            fun(*args, **kwargs)
+    return wrapper
+
 def shell():
     open_shell('cd '+ settings.GIT_DIR)
 
+@gitdir
 def deploy_dataset(dataset, list = True):
-    with cd(settings.GIT_DIR):
-        dataset_path = os.path.join(settings.WWW_DIR, dataset)
-        
-        sudo('rm -rf ' + dataset_path)
-        sudo('mkdir -p %s' % dataset_path)
-        sudo('cp datasets/'+ dataset + '/raw/* ' + dataset_path)
-        if list:
-            run('ls '+ dataset_path)
+    dataset_path = os.path.join(settings.WWW_DIR, dataset)
+    
+    sudo('rm -rf ' + dataset_path)
+    sudo('mkdir -p %s' % dataset_path)
+    sudo('cp datasets/'+ dataset + '/raw/* ' + dataset_path)
+    if list:
+        run('ls '+ dataset_path)
         
 def deploy_all():
     for dataset in settings.DATASET_LIST:
@@ -29,21 +36,21 @@ def server_restart():
 def server_stop():
     sudo('/etc/init.d/lighttpd stop')
 
+@gitdir
 def clone_repository():
-    with cd(settings.GIT_DIR):
-        run('rm -rf * .git*')
-        run('ls -a')
-        run('git clone %s .' % settings.GIT_REPO_URL)
-        run('tree -d .')
+    run('rm -rf * .git*')
+    run('ls -a')
+    run('git clone %s .' % settings.GIT_REPO_URL)
+    run('tree -d .')
         
+@gitdir
 def checkout_remote_branch(branch):
-    with cd(settings.GIT_DIR):
-        run('git branch -a')
-        run('git checkout -b %s origin/%s' % (branch,branch))
-        run('git branch')
+    run('git branch -a')
+    run('git checkout -b %s origin/%s' % (branch,branch))
+    run('git branch')
         
+@gitdir
 def checkout_branch(branch):
-    with cd(settings.GIT_DIR):
         run('git branch -a')
         run('git checkout %s' % branch)
         run('git branch')
