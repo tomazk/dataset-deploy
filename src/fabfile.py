90a6059fe21d10c7dca644b85681b4278fa49140
Tomaz Kovacic <tomaz.kovacic@gmail.com>
2011-04-15 17:03:57 +0200

    Added: logging, repo branch pulling

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,7 +1,16 @@
 import os
-from fabric.api import env, run, cd, sudo, open_shell
+import logging
+from fabric.api import env, run, cd, sudo, open_shell, put
 import settings
 
+# logging init
+log_path = os.path.join(os.getcwd(), 'fabric.log')
+logger = logging.getLogger('local_fabric')
+logger.addHandler(logging.FileHandler(filename = log_path))
+logger.addFilter(logging.Filter('local_fabric'))
+logger.setLevel(logging.DEBUG)
+
+# fabric enviroment 
 env.host_string = settings.HOST_STRING
 env.password = settings.PASS
 
@@ -12,6 +21,11 @@ def gitdir(fun):
             fun(*args, **kwargs)
     return wrapper
 
+# commands
+
+def copy_log():
+    put(log_path, os.path.join(settings.HOME, 'fabric.log'))
+    
 def shell():
     open_shell('cd '+ settings.GIT_DIR)
 
@@ -24,17 +38,21 @@ def deploy_dataset(dataset, list = True):
     sudo('cp datasets/'+ dataset + '/raw/* ' + dataset_path)
     if list:
         run('ls '+ dataset_path)
+    logger.info('deployed dataset %s to %s', dataset , dataset_path )
         
 def deploy_all():
+    logger.info('deploying all datasets')
     for dataset in settings.DATASET_LIST:
         deploy_dataset(dataset, list = False) 
     run('tree -d '+ settings.WWW_DIR)
 
 def server_restart():
     sudo('/etc/init.d/lighttpd restart')
+    logger.info('lighttpd restart')
     
 def server_stop():
     sudo('/etc/init.d/lighttpd stop')
+    logger.info('lighttpd stop')
 
 @gitdir
 def clone_repository():
@@ -42,15 +60,23 @@ def clone_repository():
     run('ls -a')
     run('git clone %s .' % settings.GIT_REPO_URL)
     run('tree -d .')
+    logger.info('cloned repository from %s', settings.GIT_REPO_URL )
         
 @gitdir
 def checkout_remote_branch(branch):
     run('git branch -a')
     run('git checkout -b %s origin/%s' % (branch,branch))
     run('git branch')
+    logger.info('checkout remote branch %s', branch)
         
 @gitdir
 def checkout_branch(branch):
-        run('git branch -a')
-        run('git checkout %s' % branch)
-        run('git branch')
+    run('git branch -a')
+    run('git checkout %s' % branch)
+    run('git branch')
+    logger.info('checkout branch %s', branch)
+        
+@gitdir
+def pull_branch(branch):
+    run('git pull origin %s' % branch)
+    logger.info('pull from branch %s', branch)
